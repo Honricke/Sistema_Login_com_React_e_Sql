e16753f7e7af5d7e67c8b4a4147bc1e1aa9b23b6
Henrique Bandeira <henriquebandeira307@gmail.com>
2022-06-17 20:04:02 -0300

    Criando Front Login

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import TextInput from './TextInput'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	function getValues(e){
+		e.preventDefault()
+		console.log(e.target[0].value)
+	}
+	return (
+		<>
+			<form onSubmit={getValues} action="#">
+				<TextInput message={["Login","text"]}/>
+				<TextInput message={["Senha","password"]}/>
+				<button className='botao' type='submit'>Enviar</button>
+			</form>
+			<a href='./cadastro.html'></a>
+		</>
+	)
 }
 
 export default App;
---
+++ app/src/TextInput.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import './TextInput.css';
+
+function TextInput(text){
+    const [valor, setvalor] = useState("");
+
+    const testValidate = (e) => {
+        setvalor(e.target.value)
+        if(e.target.value != ""){
+            e.target.setAttribute("className","valido")
+        }
+        else if(e.target.value == ""){
+            e.target.removeAttribute("className")
+        }
+    }
+    return (
+        <div id="text-container">
+            <input id={text.message[0]} type={text.message[1]} autoComplete="off" onChange={testValidate} value={valor}/>
+            <label htmlFor={text.message[0]}>{text.message[0]}</label>
+        </div>
+    )
+}
+
+export default TextInput;
